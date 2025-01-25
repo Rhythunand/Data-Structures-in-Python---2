8773d28a8140008b5741a8025645ca2ef25ae0ed
Rhythunand <rhythunandbiju@gmail.com>
2025-01-25 15:58:24 +0300

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,10 @@
+set1 = {'Green', 'Blue'}
+set2 = {'Blue', 'Yellow'}
+print("Orginal set of elements :")
+print(set1)
+print(set2)
+
+
+set_inter = set1.intersection(set2)
+print("\nIntersection of two said sets")
+print(set_inter)
