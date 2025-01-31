0d811a19970a2fc9e7b41b5a7daf09280830df90
Rhythunand <rhythunandbiju@gmail.com>
2025-01-31 15:56:05 +0300

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,10 @@
+tuple_2 = (('Name '  ' Rhythunand'), ('Last Name '  ' Biju') ,('Age '  ' 11'))
+tupl = type(tuple_2)
+
+lst = list(tuple_2)
+tst_2 = type(lst)
+
+print("\n", tuple_2)
+print("The data type of the elemnets shown above is",tupl)
+print("\n", lst)
+print("The data type of the elemnets shown above is", tst_2)
